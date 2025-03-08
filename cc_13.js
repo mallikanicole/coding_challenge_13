0b2ff912c2d3105254bed61efcef3cc82bf4a9cf
mallikanicole <malmichael23@gmail.com>
2025-03-07 21:29:10 -0500

    Task 2 - Employee Cards Dynamic Addition

--- cc_13.js
+++ cc_13.js
@@ -1,17 +1,20 @@
 //Task 2 Adding Employee Cards Dynamically
 
-function addEmployeeCard(name,position){
+function addEmployeeCard(name,position){//building employee card
     const employeeContainer=document.getElementById("employeeContainer")
     const card=document.createElement('div');
     card.setAttribute("class","employee-card");
     card.classList.add('employee-card');
 
+    //creating heading of employee card
     const employeeName=document.createElement('h2');
     employeeName.textContent=name;
 
+    //creating paragraph for employee position
     const employeePosition=document.createElement('p');
     employeePosition.textContent=position;
 
+    //creating remove button
     const removeButton=document.createElement('button');
     removeButton.textContent='Remove';
     removeButton.setAttribute("class","remove-button");
@@ -27,7 +30,10 @@ function addEmployeeCard(name,position){
 
 };
 
+//adding employees
 addEmployeeCard("Johnny Walker","Technician");
 addEmployeeCard("Jim Beam","Advisor");
 addEmployeeCard("Jack Daniels","Researcher");
 addEmployeeCard("Elijah Craig","Analyst");
+
+
